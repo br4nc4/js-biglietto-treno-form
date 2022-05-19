108464703d90e49cf5c32a14f3ca06a06902cce3
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-19 15:28:34 +0200

    (start) script pt.1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 "use strict"
+/* SELEZIONO GLI INPUT E IL BUTTON, E LI PORTO IN QUESTO FILE TRAMITE QUETSE VARIABILI */
+const inputName =document.querySelector("[name='userName']");
+const inputDistance = document.querySelector("[name='distanceKm']")
+const submitElement = document.getElementById("submitButton")
