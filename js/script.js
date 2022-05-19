7d4b4412fba09eeab58169610e99a711ba199a33
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-19 19:09:09 +0200

    test script pt.12

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const submitElement = document.getElementById("submitButton");
 const outputName = document.getElementById("outputUserName");
 const outputPriceTot = document.getElementById("outputPrice");
 const optionAge = document.querySelector("[name='option-age']");
+const outputTypeTicket = document.getElementById("outputType")
 
 
 
@@ -16,8 +17,8 @@ const sconto2 = 0.4;
 
 
 /* LE VARIABILI INIZIALMENTE DEVONO ESSERE VUOTE E SOLO SUCCESSIVAMENTE ASSUMERE IL VAOLRE NEL CALCOLO PER IL PREZZO DEL BIGLIETTO */
-let realSale = "";
-let realPrice = ""; 
+let realSale = 0;
+let realPrice = 0; 
 
 
 /* ATTRIBUISCO L'EVENTO CHE VERRà GENERATO UNA VOLTA CLICCATO IL BUTTON "GENERA" */
@@ -27,13 +28,23 @@ submitElement.addEventListener("click", function() {
     /* STABILISCO LA PRIMA CONDIZIONE */
     /* SE VIENE SELEZIONATO MINORENNE DAL MENU CON LE VARIEM OPZIONI ALLORA VIENE APPLICATO LO SCONTO AL PREZZO FINALE */
     if (optionAge.value === "minorenne"){
-        realSale = parseInt(tariffaKm * inputDistance.value * sconto1);
-        realPrice = parseInt((tariffaKm * inputDistance.value) - realSale);
+        realSale = (tariffaKm * inputDistance.value * sconto1);
+        realPrice = ((tariffaKm * inputDistance.value) - realSale);
         outputPriceTot.innerHTML = realPrice.toFixed(2) + "€";
+        outputTypeTicket.innerHTML = "Bigletto Scontato";
     }
+    /* NELLA SECONDA CONDIZIONE VIENE SELEZIONATO MAGGIORENNE E VIENE CALCOLATO IL RELATIVO PREZZO */
     else if (optionAge.value === "maggiorenne"){
         realPrice = (tariffaKm * inputDistance.value);
         outputPriceTot.innerHTML = realPrice.toFixed(2) + "€";
+        outputTypeTicket.innerHTML = "Bigletto Standard";
+    }
+    /* NELLA TERZA CONDIZIONE VIENE SELEZIONATO OVER 65 E VIENE CALCOLATO IL RELATIVO PREZZO*/
+    else if (optionAge.value === "over65"){
+        realSale = (tariffaKm * inputDistance.value * sconto2);
+        realPrice = ((tariffaKm * inputDistance.value) - realSale);
+        outputPriceTot.innerHTML = realPrice.toFixed(2) + "€";
+        outputTypeTicket.innerHTML = "Bigletto Scontato";
     }
 });
 
