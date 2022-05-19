0846800ba0cda84031b22c11f3e3f0af320c067c
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-19 17:38:02 +0200

    test script pt.8

--- js/script.js
+++ js/script.js
@@ -5,16 +5,32 @@ const inputDistance = document.querySelector("[name='distanceKm']");
 const submitElement = document.getElementById("submitButton");
 const outputName = document.getElementById("outputUserName");
 const outputPriceTot = document.getElementById("outputPrice");
+let optionAge1 = document.getElementById("option1");
 
-/* VARIABILE CHE SERVE AL CALCOLO DEL PREZZO DEL BIGLIETTO */
+
+/* VARIABILI CHE SERVONO AL CALCOLO DEL PREZZO DEL BIGLIETTO */
 const tariffaKm = 0.21;
-let realPrice = ""; /* LA VARIABILE INIZIALMENTE DEVE ESSERE VUOTA E SOLO SUCCESSIVAMENTE ASSUMERE IL VAOLRE DEL CALCOLO PE RIL PREZZO DEL BIGLIETTO */
+const sconto1 = 0.2;
+const sconto2 = 0.4;
+
+
+/* LE VARIABILI INIZIALMENTE DEVONO ESSERE VUOTE E SOLO SUCCESSIVAMENTE ASSUMERE IL VAOLRE NEL CALCOLO PER IL PREZZO DEL BIGLIETTO */
+let realPrice = ""; 
+let realSale = "";
+
+
+
+
 
 /* ATTRIBUISCO L'EVENTO CHE VERRà GENERATO UNA VOLTA CLICCATO IL BUTTON "GENERA" */
 /* L'EVENTO RIGUARDA FAR COMPARIRE IL NOME NELLA PARTE INFERIORE DELL'APP, SEZIONE NOME PASSEGGERO */
 submitElement.addEventListener("click", function() {
     outputName.innerHTML = inputName.value;
-    realPrice = (tariffaKm * inputDistance.value);
-    outputPriceTot.innerHTML = realPrice.toFixed(2) + "€";
+    if (optionAge1.value = "minorenne"){
+        realSale = (tariffaKm * inputDistance.value * sconto1);
+        realPrice = ((tariffaKm * inputDistance) - realSale);
+        outputPriceTot.innerHTML = realPrice.toFixed(2) + "€";
+    }
+    
 });
 
