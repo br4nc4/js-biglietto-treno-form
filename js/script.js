916fbcd2a46a0ad65cbfc5bccb8275b5cabbfe4d
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-19 15:57:47 +0200

    script pt.2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 "use strict"
 /* SELEZIONO GLI INPUT E IL BUTTON, E LI PORTO IN QUESTO FILE TRAMITE QUETSE VARIABILI */
-const inputName =document.querySelector("[name='userName']");
-const inputDistance = document.querySelector("[name='distanceKm']")
-const submitElement = document.getElementById("submitButton")
+const inputName = document.querySelector("[name='userName']");
+const inputDistance = document.querySelector("[name='distanceKm']");
+const submitElement = document.getElementById("submitButton");
+const outputName = document.getElementById("outputUserName")
 
 /* ATTRIBUISCO L'EVENTO CHE VERRà GENERATO UNA VOLTA CLICCATO IL BUTTON "GENERA" */
+/* L'EVENTO RIGUARDA FAR COMPARIRE IL NOME NELLA PARTE INFERIORE DELL'APP, SEZIONE NOME PASSEGGERO */
 submitElement.addEventListener("click", function() {
-
-})
+    outputName.innerHTML = inputName.value;
+});
