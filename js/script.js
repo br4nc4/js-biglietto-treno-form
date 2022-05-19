84db2a2f90811507fbaaf2e746116478f1e6a0b7
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-05-19 16:24:32 +0200

    test script pt.4

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@ let outputPriceTot = document.querySelector("outputPrice");
 
 /* VARIABILE CHE SERVE AL CALCOLO DEL PREZZO DEL BIGLIETTO */
 const tariffaKm = 0.21;
-
+outputPriceTot = tariffaKm * inputDistance.value;
 
 /* ATTRIBUISCO L'EVENTO CHE VERRà GENERATO UNA VOLTA CLICCATO IL BUTTON "GENERA" */
 /* L'EVENTO RIGUARDA FAR COMPARIRE IL NOME NELLA PARTE INFERIORE DELL'APP, SEZIONE NOME PASSEGGERO */
 submitElement.addEventListener("click", function() {
     outputName.innerHTML = inputName.value;
+    
 });
